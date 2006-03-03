977ff049cb54fbf5503f25938f1f26cebafdcab7
Michalis Kamburelis <michalis.kambi@gmail.com>
2006-03-03 05:11:30 +0000

    (continuation of previous commit...) * Some content moved from CastlePlay to CastleHelp * help_message.inc/txt renamed to castle_help_message.inc/txt, since I prefix everything with castle
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/castle/trunk@174 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ castlehelp.pas
@@ -0,0 +1,51 @@
+unit CastleHelp;
+
+interface
+
+const
+  Version = '0.2.0';
+
+procedure ShowHelpMessage;
+
+function SProgramHelpSuffix: string;
+
+procedure ViewGameMessages;
+
+implementation
+
+uses SysUtils, Classes, GLWinMessages, CastleWindow, KambiUtils, CastlePlay;
+
+procedure ShowHelpMessage;
+const
+  HelpMessage = {$I castle_help_message.inc};
+begin
+  MessageOK(Glw, HelpMessage + nl + SProgramHelpSuffix, taLeft);
+end;
+
+function SProgramHelpSuffix: string;
+begin
+  Result :=
+    ApplicationName + ' version ' + Version + '.' +nl+
+    'Author: Michalis Kamburelis, aka Kambi <michalis@camelot.homedns.org>' +nl+
+    { TODO: later I will just use here SCamelotProgramHelpSuffix,
+      for now this program is not avail on camelot. }
+    {'See http://www.camelot.homedns.org/~michalis/ for latest versions' +
+    Iff(WrapLines, nl + ' ', '') +
+    ' of this program, sources, documentation etc.' +nl+}
+    'Compiled with ' + SCompilerDescription +'.';
+end;
+
+procedure ViewGameMessages;
+var
+  SList: TStringList;
+begin
+  SList := TStringList.Create;
+  try
+    SList.Assign(GameMessages);
+    SList.Insert(0, Format('%d messages :', [GameMessages.Count]));
+    SList.Insert(1, '');
+    MessageOK(Glw, SList, taLeft);
+  finally SList.Free end;
+end;
+
+end.
