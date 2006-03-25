67465d2566162fc395416390fc238210ba4bf72f
Michalis Kamburelis <michalis.kambi@gmail.com>
2006-03-25 07:08:50 +0000

    * basic_castle improved, to be a better test:   glass prison has open backdoor,   sword and life potion are closer to the player,   scrolls of flying are lying near the entrance to the tower,   additional aliens: inside the tower, in the river, on top of the tower,   camera start pos changed * sword damage lowered 2x, to make it harder to kill alien
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/castle/trunk@375 afe8dcc5-9094-451b-8773-5f495042a737

--- castleitems.pas
+++ castleitems.pas
@@ -603,7 +603,7 @@ begin
     if Boxes3dCollision(C.BoundingBox, WeaponBoundingBox) then
     begin
       { TODO: some "throw back" for creature here ? }
-      C.Life := C.Life - 40 - Random(40);
+      C.Life := C.Life - 20 - Random(20);
       C.LastAttackDirection := Player.Navigator.CameraDir;
     end;
   end;
