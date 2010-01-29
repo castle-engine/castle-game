9572273e74588f0bca93752c96f6a4f6bfbab05d
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-01-29 01:59:51 +0000

    * TKamSceneManager.Render3D / OnRender3D had inverted InShadow/TransparentGroup parameter order than TBase3D.Render.   Which was correct, but confusing.   Fixed now (changed TKamSceneManager versions). TransparentGroup is before InShadow now, which seems better (most implementation need to account for TransparentGroup, while for InShadow --- only those that need to work with shadow volumes).
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle@5376 afe8dcc5-9094-451b-8773-5f495042a737

--- source/castleplay.pas
+++ source/castleplay.pas
@@ -381,7 +381,7 @@ end;
 type
   TRenderer = class
     class procedure RenderCreaturesItems(TransparentGroup: TTransparentGroup);
-    class procedure RenderLevel(InShadow: boolean; TransparentGroup: TTransparentGroup);
+    class procedure RenderLevel(TransparentGroup: TTransparentGroup; InShadow: boolean);
     class procedure RenderShadowVolumes;
   end;
 
@@ -406,7 +406,7 @@ begin
   Level.RenderShadowVolume(SV);
 end;
 
-class procedure TRenderer.RenderLevel(InShadow: boolean; TransparentGroup: TTransparentGroup);
+class procedure TRenderer.RenderLevel(TransparentGroup: TTransparentGroup; InShadow: boolean);
 begin
   if InShadow then Level.PushLightsOff;
   try
@@ -421,7 +421,7 @@ procedure Draw(Glwin: TGLWindow);
   procedure RenderNoShadows;
   begin
     TRenderer.RenderCreaturesItems(tgOpaque);
-    TRenderer.RenderLevel(false, tgAll);
+    TRenderer.RenderLevel(tgAll, false);
     { Rendering order of Creatures, Items and Level:
       You know the problem. We must first render all non-transparent objects,
       then all transparent objects. Otherwise transparent objects
