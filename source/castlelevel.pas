0bd985620f8cc32b9730a887c0296a93e2156722
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-08-16 19:59:19 +0000

    Fixes after inventing TVRMLShape.Geometry as returning Proxy:
    * in castle and malfunction, the trick with removing must be fixed now
      to remove NodeFreeRemovingFromAllParents(LevelBoxShape.OriginalGeometry)
    * in precompute_radiance_transfer.lpr, no point in calculating PRT
      if only Proxy is coord-based.
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle@6397 afe8dcc5-9094-451b-8773-5f495042a737

--- source/castlelevel.pas
+++ source/castlelevel.pas
@@ -1491,7 +1491,7 @@ begin
       as it should not be visible).
       This way we can comfortably set such boxes from Blender. }
     Box := BoxShape.BoundingBox;
-    MainScene.NodeFreeRemovingFromAllParents(BoxShape.Geometry);
+    MainScene.NodeFreeRemovingFromAllParents(BoxShape.OriginalGeometry);
   end;
 end;
 
