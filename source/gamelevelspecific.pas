e6003242b3b5f26206cd2c6fd13269427c7a2c04
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-08-06 22:33:47 +0000

    Rename ThunderEffect to just Thunder.
    Also rename ThunderLightNode and ThunderLight without "Thunder" prefix, these are fields inside TThunder anyway.
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle@11648 afe8dcc5-9094-451b-8773-5f495042a737

--- source/gamelevelspecific.pas
+++ source/gamelevelspecific.pas
@@ -806,10 +806,10 @@ constructor TCagesLevel.Create(AOwner: TComponent; AWorld: T3DWorld;
 begin
   inherited;
 
-  ThunderEffect := TThunderEffect.Create;
-  ThunderEffect.AmbientIntensity := 0.5;
-  ThunderEffect.Color := Vector3Single(0.5, 0.5, 1);
-  ThunderEffect.Direction := Vector3Single(0, -1, 1);
+  Thunder := TThunder.Create;
+  Thunder.AmbientIntensity := 0.5;
+  Thunder.Color := Vector3Single(0.5, 0.5, 1);
+  Thunder.Direction := Vector3Single(0, -1, 1);
 
   SpidersAppearing := T3DList.Create(Self);
   AWorld.Add(SpidersAppearing);
