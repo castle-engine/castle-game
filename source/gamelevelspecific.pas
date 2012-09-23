52525e52c0401b05c8fb652fe886564af254c53c
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-09-23 19:44:26 +0000

    Rename unit name CastleShape to CastleShapes. Since we renamed in anyway (Shape->CastleShape), make it consistent (plural) with other names. You usually have many shapes.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle@11881 afe8dcc5-9094-451b-8773-5f495042a737

--- source/gamelevelspecific.pas
+++ source/gamelevelspecific.pas
@@ -25,7 +25,7 @@ unit GameLevelSpecific;
 
 interface
 
-uses CastleScene, Boxes3D, VectorMath, CastleShape, CastlePlayer, CastleLevels,
+uses CastleScene, Boxes3D, VectorMath, CastleShapes, CastlePlayer, CastleLevels,
   GameSound, X3DNodes, DOM, Base3D, PrecalculatedAnimation, CastleSoundEngine,
   GameCreatures, CastleCreatures, Classes, CastleTimeUtils, CastleColors, Frustum;
 
