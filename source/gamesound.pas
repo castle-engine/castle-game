1daf0b50eef56e6b50d59e2e6b32e167a5e4a884
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-11-12 00:34:52 +0000

    Remove many useless variables from GameSound.
    Sounds configured through xml files do not need to be listed here.
    
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle@11987 afe8dcc5-9094-451b-8773-5f495042a737

--- source/gamesound.pas
+++ source/gamesound.pas
@@ -40,22 +40,13 @@ var
 
   { Items sounds.
     @groupBegin }
-  stSwordEquipping,
-  stSwordAttackStart,
-  stArrowFired,
-  stArrowHit,
   stKeyUse,
   stKeyDoorUse,
-  stBowAttackStart,
-  stBowEquipping,
   { @groupEnd }
 
   { Levels sounds.
     @groupBegin }
-  stGateMusic,
   stCastleHallSymbolMoving,
-  stCastleHallMusic,
-  stCagesMusic,
   stStairsBlockerDestroyed,
   stTeleport,
   stSacrilegeAmbush,
@@ -70,17 +61,8 @@ var
 
   { Creatures sounds.
     @groupBegin }
-  stWerewolfActualAttackHit,
   stWerewolfHowling,
-  stBallMissileFired,
-  stBallMissileExplode,
-  stBallMissileIdle,
-  stSpiderActualAttackHit,
   stSpiderAppears,
-  stSpiderQueenActualAttackHit,
-  stThrownWebFired,
-  stThrownWebHit,
-  stThrownWebIdle,
   { @groupEnd }
 
   { Others.
@@ -101,18 +83,9 @@ initialization
 
   stPlayerPotionDrink          := SoundEngine.SoundFromName('player_potion_drink');
   stPlayerCastFlyingSpell      := SoundEngine.SoundFromName('player_cast_flying_spell');
-  stSwordEquipping             := SoundEngine.SoundFromName('sword_equipping');
-  stSwordAttackStart           := SoundEngine.SoundFromName('sword_attack_start');
-  stArrowFired                 := SoundEngine.SoundFromName('arrow_fired');
-  stArrowHit                   := SoundEngine.SoundFromName('arrow_hit');
   stKeyUse                     := SoundEngine.SoundFromName('key_use');
   stKeyDoorUse                 := SoundEngine.SoundFromName('key_door_use');
-  stBowAttackStart             := SoundEngine.SoundFromName('bow_equipping');
-  stBowEquipping               := SoundEngine.SoundFromName('bow_attack_start');
-  stGateMusic                  := SoundEngine.SoundFromName('gate_music');
   stCastleHallSymbolMoving     := SoundEngine.SoundFromName('castle_hall_symbol_moving');
-  stCastleHallMusic            := SoundEngine.SoundFromName('castle_hall_music');
-  stCagesMusic                 := SoundEngine.SoundFromName('cages_music_with_rain');
   stStairsBlockerDestroyed     := SoundEngine.SoundFromName('stairs_blocker_destroyed');
   stTeleport                   := SoundEngine.SoundFromName('teleport');
   stSacrilegeAmbush            := SoundEngine.SoundFromName('sacrilege_ambush');
@@ -123,17 +96,8 @@ initialization
   stElevator                   := SoundEngine.SoundFromName('elevator');
   stDoomExitButton             := SoundEngine.SoundFromName('doom_exit_button');
   stCreak                      := SoundEngine.SoundFromName('creak');
-  stWerewolfActualAttackHit    := SoundEngine.SoundFromName('werewolf_actual_attack_hit');
   stWerewolfHowling            := SoundEngine.SoundFromName('werewolf_howling');
-  stBallMissileFired           := SoundEngine.SoundFromName('ball_missile_fired');
-  stBallMissileExplode         := SoundEngine.SoundFromName('ball_missile_explode');
-  stBallMissileIdle            := SoundEngine.SoundFromName('ball_missile_idle');
-  stSpiderActualAttackHit      := SoundEngine.SoundFromName('spider_actual_attack_hit');
   stSpiderAppears              := SoundEngine.SoundFromName('spider_appears');
-  stSpiderQueenActualAttackHit := SoundEngine.SoundFromName('spider_queen_actual_attack_hit');
-  stThrownWebFired             := SoundEngine.SoundFromName('thrown_web_fired');
-  stThrownWebHit               := SoundEngine.SoundFromName('thrown_web_hit');
-  stThrownWebIdle              := SoundEngine.SoundFromName('thrown_web_idle');
   stIntroMusic                 := SoundEngine.SoundFromName('intro_music');
   stSaveScreen                 := SoundEngine.SoundFromName('save_screen');
   stGameWinMusic               := SoundEngine.SoundFromName('game_win_music');
