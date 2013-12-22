f601476b6830e664439710d0d6545197d91392e5
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-12-22 19:31:40 +0000

    Fix gameinputs.pas compilation.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/castle@13477 afe8dcc5-9094-451b-8773-5f495042a737

--- source/gameinputs.pas
+++ source/gameinputs.pas
@@ -37,7 +37,7 @@ var
 
 implementation
 
-uses CastleKeysMouse, CastleSceneManager;
+uses CastleKeysMouse, CastleSceneManager, CastlePlayer;
 
 initialization
   { Change defaults, to make "attack" on mouse click.
